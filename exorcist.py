5799f8c4d700fa7e22ef0b8280b00dd24a3c8981
Mike Moss <mrmoss@alaska.edu>
2015-11-24 09:53:47 -0900

    HTTP file carving might actually be working now!

--- exorcist.py
+++ exorcist.py
@@ -7,63 +7,42 @@ import pefile
 import pprint
 from scapy.all import *
 
-#parser = PcapReader("test.pcap")
-#
-#for p in parser:
-#	test=str(p.payload)
-#	count=0
-#	pos=0
-#
-#	while pos>=0:
-#		pos=test.find("\x0d\x0a\x32\x30\x30\x30\x0d\x0a",pos)
-#
-#		if pos>0:
-#			count+=1
-#			pos+=8
-#
-#	if count>0:
-#		print(test)
-
-
-
 #returns [(stream,header,http_payload)]
 def get_http_https(streams):
 	ret=[]
 
 	for ii in range(0,len(streams)):
 		start_pos=0
+		raw=streams[ii][1]+"\r\n\r\n"
 
-		try:
-			streams[ii][1].index("\r\n\r\n")
-			payload=streams[ii][1]+"\r\n\r\n"
+		while raw.find("\r\n\r\n",start_pos)>=0:
+			end_pos=raw.index("\r\n\r\n",start_pos)
+			header=raw[start_pos:end_pos]
+			payload=""
+			end_pos+=4
 
-			while True:
+			if len(header)>4 and header[0:4]=="HTTP":
 				try:
-					end_pos=payload.index("\r\n\r\n",start_pos)
-
-					header=payload[start_pos:end_pos]
 					header=dict(re.findall(r'(?P<name>.*?):(?P<value>.*?)\r\n',header))
 					header=dict((key.lower(),value) for key,value in header.iteritems())
-					start_pos=end_pos+4
 
-					if header["transfer-encoding"].strip()=="chunked":
-						#print(":".join("{:02x}".format(ord(c)) for c in payload[start_pos:start_pos+6]))
-						end_pos=payload.find("\r\n",start_pos)
-						new_size=int(payload[start_pos:end_pos],16)
-						start_pos=end_pos+2
-						end_pos=start_pos+new_size
-					else:
-						end_pos=start_pos+int(header["content-length"])
+					if "transfer-encoding" in header and header["transfer-encoding"].strip()=="chunked":
+						while raw.find("\r\n",end_pos)>end_pos:
+							chunk_size=int(raw[end_pos:raw.find("\r\n",end_pos)],16)
+							end_pos=raw.find("\r\n",end_pos)+2
+							payload+=raw[end_pos:end_pos+chunk_size]
+							end_pos+=chunk_size
+					elif "content-length" in header:
+						size=int(header["content-length"].strip())
+						payload=raw[end_pos:end_pos+size]
+						end_pos+=size
+				except:
+					pass
+				else:
+					if len(payload)>0:
+						ret.append((streams[ii],header,payload));
 
-					data=payload[start_pos:end_pos]
-					end_pos+=1
-
-					ret.append((streams[ii],header,data));
-
-				except Exception as e:
-					break
-		except:
-			pass
+			start_pos=end_pos
 
 	return ret
 
@@ -142,14 +121,8 @@ def get_streams(filename):
 			if TCP in packet and type(packet[TCP].payload)==Raw:
 				packet_payload=str(packet[TCP].payload)
 				payload+=packet_payload
-				#print("BEFORE "+str(packet_payload.find("\r\n2000\r\n")))
-				#packet_payload=packet_payload.replace("\r\n2000\r\n","");
-				#print("AFTER  "+str(packet_payload.find("\r\n2000\r\n")))
-				#payload_chunked+=packet_payload
-				#print("OVERALL"+str(payload_chunked.find("\r\n2000\r\n")))
 
 		ret.append((ii,payload));
-		#ret.append((ii,payload_chunked.replace("\r\n2000\r\n","").replace("\r\n20a0\r\n","")));
 
 	return ret
 
