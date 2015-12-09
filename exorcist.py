45719968b843266773b39315eab91ddd6136dba7
Mike Moss <mrmoss@alaska.edu>
2015-12-09 12:07:08 -0900

    Cleanup.

--- exorcist.py
+++ exorcist.py
@@ -28,25 +28,30 @@ def carve_http(streams):
 				try:
 					header=dict(re.findall(r'(?P<name>.*?):(?P<value>.*?)\r\n',header))
 					header=dict((key.lower(),value) for key,value in header.iteritems())
+
 					if "content-type" in header:
 						mime=str(header["content-type"].strip())
+
 						if mime.find(";")>-1:
 							mime=mime[0:mime.index(";")]
 
 					if "transfer-encoding" in header and header["transfer-encoding"].strip()=="chunked":
 						while raw.find("\r\n",end_pos)>=0:
 							chunk_size=raw[end_pos:raw.find("\r\n",end_pos)]
-							if len(chunk_size)>0:
-								chunk_size=int(chunk_size,16)
-								end_pos=raw.find("\r\n",end_pos)+2
-								carving+=raw[end_pos:end_pos+chunk_size]
-								end_pos+=chunk_size+2
-							else:
+
+							if len(chunk_size)<=0:
 								break
+
+							chunk_size=int(chunk_size,16)
+							end_pos=raw.find("\r\n",end_pos)+2
+							carving+=raw[end_pos:end_pos+chunk_size]
+							end_pos+=chunk_size+2
+
 					elif "content-length" in header:
 						size=int(header["content-length"].strip())
 						carving=raw[end_pos:end_pos+size]
 						end_pos+=size
+
 				except:
 					pass
 
@@ -64,10 +69,12 @@ def save_carvings(carvings,out,count_start=0):
 	try:
 		for carving in carvings:
 			mime=carving[2]
+
 			if mime=="":
 				mime=magic.from_buffer(carving[1],mime=True)
 
 			extension=str(mimetypes.guess_extension(mime))
+
 			if mime=="text/javascript" or mime=="application/x-javascript":
 				extension=".js"
 			if mime=="text/html":
@@ -95,8 +102,10 @@ def save_carvings(carvings,out,count_start=0):
 			file_folder=file_folder.replace(" ","_")
 			file_folder=file_folder.replace(">","TO")
 			file_path=out+"/"+file_folder+"/"
+
 			if not os.path.isdir(file_path):
 				os.makedirs(file_path)
+
 			print("\tSaving \""+file_path+str(count)+extension+"\"")
 			file=open(file_path+str(count)+extension,'w')
 			file.write(carving[1])
@@ -106,6 +115,7 @@ def save_carvings(carvings,out,count_start=0):
 	except Exception as error:
 		print(error)
 		raise Exception("Error saving files.")
+
 	finally:
 		return count
 
@@ -119,12 +129,16 @@ def get_streams(filename):
 		for session in sessions:
 			payload=""
 			payload_chunked=""
+
 			for packet in sessions[session]:
 				if TCP in packet and type(packet[TCP].payload)==Raw:
 					packet_payload=str(packet[TCP].payload)
 					payload+=packet_payload
+
 			ret.append((session,payload));
+
 		return ret
+
 	except:
 		raise Exception("Error opening pcap \""+filename+"\".")
 
@@ -138,9 +152,11 @@ if __name__=="__main__":
 	for ii in range(1,len(sys.argv)):
 		filename=str(sys.argv[ii])
 		print("Processing \""+filename+"\"")
+
 		try:
 			streams=get_streams(filename)
 			carvings=carve_http(streams)
 			files_wrote=save_carvings(carvings,"out/"+filename,files_wrote)
+
 		except Exception as error:
 			print(error)
